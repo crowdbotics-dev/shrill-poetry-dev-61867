7b0b2508fcf5498759224c260726638c5f967c5f
Crowdbotics <engineering@crowdbotics.com>
2023-02-07 16:05:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-poetry-dev-61867",
+  url: "https://shrill-poetry-dev-61867.botics.co",
+  api: "https://shrill-poetry-dev-61867.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
